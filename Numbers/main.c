46d2a191565749f4b8cf788043607449b4491ab0
dhabhai-srlife <hdhabhai@srlife.net>
2018-04-02 16:22:44 -0400

    Initial Commit

---
+++ Numbers/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Numbers
+//
+//  Created by ITMAC1 on 4/2/18.
+//  Copyright © 2018 Harsh Dhabhai. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
